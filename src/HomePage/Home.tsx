99561311aca4193280c70780d80f87b0b859b57c
mikeGLA <mikecommand_conquer@hotmail.com>
2022-05-17 15:03:20 +0700

    update

--- src/HomePage/Home.tsx
+++ src/HomePage/Home.tsx
@@ -30,12 +30,19 @@ const Home: React.FC = () => {
               <div className="MyPic">
                 <img src={ProPic} width="350px" height="400px" />
               </div>
-              <div className="Introduction-Box">
-                <div>aa</div>
-                <div>bb</div>
-                <div>cc</div>
-                <div>dd</div>
-                <div>ee</div>
+              <div className="IntroductionBox">
+                <div className="IntroductionBox-Subject">
+                  <div>Name:</div>
+                  <div>Address:</div>
+                  <div>Contacts</div>
+                  <div>Git</div>
+                </div>
+                <div className="IntroductionBox-Data">
+                  <div>Thanapol Chinkangwanwong (Mike)</div>
+                  <div>95/1 Moo 3, Tambon Chaisatharn, Amphoe Saraphi, Chiang Mai, Thailand, 50140.</div>
+                  <div>thanapol.chinkangwanwong@gmail.com / 0834784611</div>
+                  <a href="https://github.com/mikeGLA" target="_blank" rel="noopener noreferrer">https://github.com/mikeGLA</a>
+                </div>
               </div>
             </div>
           </div>
